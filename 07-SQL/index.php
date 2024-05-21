2107ec60e77e9230c52b71ae1e3d8b90005a1f3d
afsanchez04 <afsanchez04@gmail.com>
2024-05-21 17:08:41 -0500

    feat: sql

---
+++ 07-SQL/index.php
@@ -0,0 +1,26 @@
+<?php
+
+//Documentación: SQL W3Schools
+
+//1. SQL Select
+//SELECT materia FROM profesores;
+//Selecciona todos los registros de materia dentro de la tabla profesores.
+
+//2. SQL Select Distinct
+//Ejemplo...
+//Definición
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
